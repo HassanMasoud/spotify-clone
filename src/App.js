14e1f088ba8ceed23a66c241c5030edf8ec333da
Hassan Muhashamy <hassan.muhashamy@gmail.com>
2020-09-11 23:42:18 +0300

    Get access token from url

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import Login from "./Login";
+import { getTokenFromUrl } from "./spotify";
 
 function App() {
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const token = hash.access_token;
+    console.log("Token: ", token);
+  }, []);
+
   return (
     <div className="app">
       <Login />
