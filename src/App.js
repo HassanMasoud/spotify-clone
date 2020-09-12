912f5d0e40a14a8e7f310f2d079a16f8d7de834c
Hassan Muhashamy <hassan.muhashamy@gmail.com>
2020-09-12 09:54:09 +0300

    Set user

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import "./App.css";
 import Login from "./Login";
 import { getTokenFromUrl } from "./spotify";
 import SpotifyWebApi from "spotify-web-api-js";
+import Player from "./Player";
+import { useDataLayerValue } from "./DataLayer";
 
 const spotify = new SpotifyWebApi();
 
 function App() {
   const [token, setToken] = useState(null);
+  const [{ user }, dispatch] = useDataLayerValue();
 
   useEffect(() => {
     const hash = getTokenFromUrl();
@@ -20,14 +23,17 @@ function App() {
       spotify.setAccessToken(_token);
 
       spotify.getMe().then((user) => {
-        console.log(user);
+        dispatch({
+          type: "SET_USER",
+          user: user,
+        });
       });
     }
   }, []);
 
-  return (
-    <div className="app">{token ? <h1>You're logged in</h1> : <Login />}</div>
-  );
+  console.log("🙃", user);
+
+  return <div className="app">{token ? <Player /> : <Login />}</div>;
 }
 
 export default App;
