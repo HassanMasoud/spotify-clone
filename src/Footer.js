5c172c8393d43646a1c52c3c2a9dae7e673d740f
Hassan Muhashamy <hassan.muhashamy@gmail.com>
2020-09-13 18:16:50 +0300

    Create footer right side

--- src/Footer.js
+++ src/Footer.js
@@ -1,10 +1,13 @@
 import React from "react";
 import "./Footer.css";
+import { Grid, Slider } from "@material-ui/core";
 import PlayCircleOutlineIcon from "@material-ui/icons/PlayCircleOutline";
 import SkipPreviousIcon from "@material-ui/icons/SkipPrevious";
 import SkipNextIcon from "@material-ui/icons/SkipNext";
 import ShuffleIcon from "@material-ui/icons/Shuffle";
 import RepeatIcon from "@material-ui/icons/Repeat";
+import PlaylistPlayIcon from "@material-ui/icons/PlaylistPlay";
+import VolumeDownIcon from "@material-ui/icons/VolumeDown";
 
 function Footer() {
   return (
@@ -20,7 +23,17 @@ function Footer() {
         <RepeatIcon className="footer__green" />
       </div>
       <div className="footer__right">
-        <p>Volume controls</p>
+        <Grid container spacing={2}>
+          <Grid item>
+            <PlaylistPlayIcon />
+          </Grid>
+          <Grid item>
+            <VolumeDownIcon />
+          </Grid>
+          <Grid item xs>
+            <Slider />
+          </Grid>
+        </Grid>
       </div>
     </div>
   );
