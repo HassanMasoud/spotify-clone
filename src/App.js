54511c7f36e6053ae22163823cf912b88e751dae
Hassan Muhashamy <hassan.muhashamy@gmail.com>
2020-09-13 20:43:17 +0300

    Build discover weekly banner

--- src/App.js
+++ src/App.js
@@ -37,6 +37,13 @@ function App() {
           playlists: playlists,
         });
       });
+
+      spotify.getPlaylist("37i9dQZEVXcGPHkuDB6WEh").then((response) => {
+        dispatch({
+          type: "SET_DISCOVER_WEEKLY",
+          discover_weekly: response,
+        });
+      });
     }
   }, []);
 
--- src/Body.js
+++ src/Body.js
@@ -1,11 +1,31 @@
 import React from "react";
 import "./Body.css";
 import Header from "./Header";
+import { useDataLayerValue } from "./DataLayer";
+import PlayCircleFilledIcon from "@material-ui/icons/PlayCircleFilled";
+import Favoriteicon from "@material-ui/icons/Favorite";
+import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
 
 function Body({ spotify }) {
+  const [{ discover_weekly }, dispatch] = useDataLayerValue();
   return (
     <div className="body">
       <Header spotify={spotify} />
+      <div className="body__info">
+        <img src={discover_weekly?.images[0].url} alt="" />
+        <div className="body__infoText">
+          <strong>PLAYLIST</strong>
+          <h2>Discover Weekly</h2>
+          <p>{discover_weekly?.description}</p>
+        </div>
+      </div>
+      <div className="body__songs">
+        <div className="body__icons">
+          <PlayCircleFilledIcon className="body__shuffle" />
+          <Favoriteicon />
+          <MoreHorizIcon />
+        </div>
+      </div>
     </div>
   );
 }
